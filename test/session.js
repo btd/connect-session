8528e159c0137a3b4acb80d6fff6c482edcf7d17
Denis Bardadym <bardadymchik@gmail.com>
2012-12-20 14:25:53 +0400

    load session id from req

---
+++ test/session.js
@@ -0,0 +1,54 @@
+var should = require('should'),
+		connect = require('connect'),
+		request = require('supertest');
+
+var session = require('../lib/session')
+
+var header = function(options) {
+	var options = options || {},
+			header = (options.header || 'X-User-Session').toLowerCase();
+
+	return function(req) {
+		return req.headers[header];
+	}
+}
+
+var app = connect()
+	.use(session([header()]))
+	.use(function(req, res){
+		if(req.url === '/get') {
+			res.end(req.session.property);
+		} else {
+			req.session.property = 'Some value';
+			res.end(req.sessionID);
+		}
+    
+  });
+
+describe('session', function() {
+	it('should find sid from header', function(done) {
+
+		var sid = false;
+
+		//first we are getting SID from API call
+		request(app)
+			.get('/')
+      .expect(200, function(err, res){
+        if (err) return done(err);
+        sid = res.text;
+
+		    request(app)
+		    	.get('/')
+		    	.set('X-User-Session', sid)
+		    	.expect(200, sid, function(err, res) {
+		    		if(err) return done(err);
+
+		    		request(app)
+		    			.get('/get')
+		    			.set('X-User-Session', sid)
+		    			.expect(200, 'Some value', done);
+		    	});
+      });
+    
+	})
+})
